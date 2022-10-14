7d4bef4f2bd5804282cd322c09a43896c4c63e60
Joshua Joseph <60847326+joshuajoseph-stu@users.noreply.github.com>
2022-10-14 14:28:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-const http = require("http");
+const express = require("express");
+const app = express();
+var bodyParser = require("body-parser");
+app.use(express.json());
+app.use(express.urlencoded());
 
-const hostname = "0.0.0.0";
-const port = 8081;
+GetRes = { todo_name: "Hello", todo_desc: "World" };
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.send("Hello World");
+app.get("/", (req, res) => {
+  res.send(GetRes);
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
 });
+
+app.listen(3000, function () {
+  console.log("listening on 3000");
+});
+
+//npm start
