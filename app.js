705f1b01b71a5b5716e3c223dd2a2906d0683a9f
Joshua Joseph <60847326+joshuajoseph-stu@users.noreply.github.com>
2022-10-14 14:24:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const http = require("http");
 
-const hostname = "127.0.0.1";
+const hostname = "0.0.0.0";
 const port = 8081;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+  res.send("Hello World");
 });
 
 server.listen(port, hostname, () => {
